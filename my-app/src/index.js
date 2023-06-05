e2e9f79ea3d43d79a07df452407d366a53bf483a
MigueldeHaroce <migueldeharoce@gmail.com>
2023-06-05 02:06:46 +0200

    Adding a time selector and finishing the timer

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,26 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
+const { app, ipcMain } = require('electron');
 const path = require('path');
+const { BrowserWindow } = require('electron-acrylic-window');
+const sound = require("sound-play");
 
 let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 1000,
-    height: 370,
+    width: 455,
+    height: 370,// Enable transparent background
+    frame: false, 
     webPreferences: {
       nodeIntegration: true,
-      preload: path.join(__dirname, 'preload.js')
-    }
+      preload: path.join(__dirname, 'preload.js'),
+      experimentalFeatures: true
+    },
+    vibrancy: {
+      theme: '#11192826', // (default) or 'dark' or '#rrggbbaa'
+      effect: 'acrylic', // (default) or 'blur'
+      disableOnBlur: true, // (default)
+    },
   });
-
   mainWindow.loadFile(path.join(__dirname, 'selectTime.html'));
 
   mainWindow.on('closed', () => {
@@ -35,7 +43,7 @@ app.on('activate', () => {
 });
 
 ipcMain.on('start-timer', (event, duration) => {
-  const endTime = new Date().getTime() + duration * 60 * 1000;
+  let endTime = new Date().getTime() + duration * 60 * 1000;
 
   BrowserWindow.getFocusedWindow().loadURL(`file://${__dirname}/index.html?duration=${duration}`);
 
@@ -44,16 +52,46 @@ ipcMain.on('start-timer', (event, duration) => {
   const updateTimer = () => {
     const currentTime = new Date().getTime();
     const remainingTime = endTime - currentTime;
-
+  
     console.log(remainingTime);
-
+  
     if (remainingTime > 0) {
-      BrowserWindow.getFocusedWindow().webContents.send('update-timer', remainingTime);
-      setTimeout(updateTimer, 1000); // Update the timer every second
-    } else {
-      BrowserWindow.getFocusedWindow().webContents.send('timer-complete');
-    }
-  };
-  updateTimer();
+      let focusedWindow = BrowserWindow.getFocusedWindow();
+      if (!focusedWindow) {
+        const windows = BrowserWindow.getAllWindows();
+        if (windows.length > 0) {
+          focusedWindow = windows[0];
+        }
+      }
+  
+      if (focusedWindow) {
+        focusedWindow.webContents.send('update-timer', remainingTime);
+      }
 
+      if (remainingTime <= 4500 && remainingTime >= 4000) {
+        const filePath = path.join(__dirname, "sound.mp3");
+        sound.play(filePath);
+      }
+    } else {
+      console.log('Timer has reached 0');
+  
+      // Timer has reached 0, start over with a new duration
+      const newEndTime = new Date().getTime() + duration * 60 * 1000;
+  
+      const windows = BrowserWindow.getAllWindows();
+      if (windows.length > 0) {
+        windows[0].loadURL(`file://${__dirname}/index.html?duration=${duration}`);
+      }
+  
+      event.sender.send('durationSelected', duration);
+  
+      // Start the timer again with the new duration
+      endTime = newEndTime;
+    }
+  
+    setTimeout(updateTimer, 500); // Update the timer every second
+  };
+  
+  updateTimer();
+  
 });
